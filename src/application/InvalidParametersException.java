e9621a92434fba6199d662aa65f7d7242f66d3c4
Gisele Araújo <araujocgisele@gmail.com>
2023-08-24 11:36:31 -0300

    Project created

---
+++ src/application/InvalidParametersException.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.Scanner;
+
+public class InvalidParametersException {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.??;
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.??;
+	}
+}
