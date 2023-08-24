a08558216b9cb3d91bc917505cf9a27e98bfe2ab
Gisele Araújo <araujocgisele@gmail.com>
2023-08-24 11:57:38 -0300

    exception and main

--- src/application/InvalidParametersException.java
+++ src/application/InvalidParametersException.java
@@ -1,13 +1,10 @@
 package application;
 
-import java.util.Scanner;
+public class InvalidParametersException extends Exception{
 
-public class InvalidParametersException {
-	public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.??;
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
+	private static final long serialVersionUID = 1L;
+
+	public InvalidParametersException(String message) {
+		super(message);
 	}
 }
